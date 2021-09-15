98bc074e5def5cd038e07ae8c8bf538c5388c499
Yuxiang <Yuxiang.L.Qin@gmail.com>
2021-09-15 22:30:21 +1000

    update gamma_CDG

--- src/core/magnitudes.c
+++ src/core/magnitudes.c
@@ -417,7 +417,7 @@ void get_output_magnitudes(float* mags, float* dusty_mags, galaxy_t* gal, int sn
     }
 
     // Best fit dust--gas model from Qiu, Mutch, da Cunha et al. 2019, MNRAS, 489, 1357
-    double factor = pow(calc_metallicity(gal->ColdGas, gal->MetalsColdGas) / 0.02, 1.2) * gal->ColdGas *
+    double factor = pow(calc_metallicity(gal->ColdGas, gal->MetalsColdGas) / 0.02, 0.65) * gal->ColdGas *
                     pow(gal->DiskScaleLength * 1e3, -2.0) * exp(-0.35 * redshift);
     dust_params_t dust_params = { .tauUV_ISM = 13.5 * factor,
                                   .nISM = -1.6,
