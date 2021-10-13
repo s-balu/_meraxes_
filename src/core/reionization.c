7580a4b6c8fac0ac0e61c9c6b41718d4265996cb
Yuxiang <Yuxiang.L.Qin@gmail.com>
2021-10-13 18:23:34 +1100

    I meant I temporarily went back to 1e9, now change back to 1e10 to  run models consistent on gadi

--- src/core/reionization.c
+++ src/core/reionization.c
@@ -58,9 +58,9 @@ void update_galaxy_fesc_vals(galaxy_t* gal, double new_stars, int snapshot)
       else
         fesc = 1.0;
       break;*/
-    case 5: // halo mass (1e9 Msun)
+    case 5: // halo mass (1e10 Msun)
       if (gal->Mvir > 0.0)
-        fesc *= pow(gal->Mvir * 10. / run_globals.params.Hubble_h, params->EscapeFracPropScaling);
+        fesc *= pow(gal->Mvir / run_globals.params.Hubble_h, params->EscapeFracPropScaling);
       else
         fesc = 1.0;
       break;
